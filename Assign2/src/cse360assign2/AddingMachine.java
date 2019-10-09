7dc615b4823543435b14afae74b4341da6fbd349
Tejasai Kanuri <Tejasai Kanuri@Sai>
2019-10-09 14:30:42 -0700

    Second commit with finished code

--- Assign2/src/cse360assign2/AddingMachine.java
+++ Assign2/src/cse360assign2/AddingMachine.java
@@ -3,25 +3,37 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history = "0";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total = total + value;
+		toHistory('a', value);
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value;
+		toHistory('s', value);
+	}
+	
+	public void toHistory (char action, int value) {
+		if (action == 'a') {
+			history = history.concat(" + "+value);	
+		}
+		if (action == 's') {
+			history = history.concat(" - "+value);
+		}
 	}
 		
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	public void clear() {
